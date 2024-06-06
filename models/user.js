9b817df5eee267d6ef0a866006a774dd1dfd8d4a
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-06-06 18:18:14 +0530

    add: user model

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const { default: mongoose } = require('mongoose');
+const bcryptjs = require('bcryptjs');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema(
+  {
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    admin: { type: Boolean, required: true },
+  },
+  { timestamps: true }
+);
+
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcryptjs.genSalt(10);
+  this.password = await bcryptjs.hash(this.password, salt);
+  next();
+});
+
+UserSchema.method.comparePassword = async (password) => {
+  return await bcryptjs.compare(password, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
