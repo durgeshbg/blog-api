8edaa45f5d3cc63224146dfb12cd0d960a889d52
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-06-06 20:55:00 +0530

    update: use blog & comment router

--- app.js
+++ app.js
@@ -34,6 +34,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookiParser());
 
+app.use('/blogs', require('./routes/blog'));
+app.use('/blogs/:id/comments', require('./routes/comment'));
+
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port: ${process.env.PORT}`);
 });
