7005b7092ed0a9fcfaf67d7f88f014527394af9d
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-06-06 22:32:59 +0530

    add: get_comments controller

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,3 +1,7 @@
-exports.get_comments = async function (req, res, next) {
-  res.json({ comments: 'TODO: get all comments' });
-};
+const Comment = require('../models/comment');
+const asyncHandler = require('express-async-handler');
+
+exports.get_comments = asyncHandler(async function (req, res, next) {
+  const comments = await Comment.find({ post: req.params.id }, { __v: 0 }).exec();
+  res.json({ comments: comments });
+});
