7723babb19ea617e76e1aa2971f0657e7a01a46f
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-06-06 07:43:30 +0530

    project init

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cookiParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limiter');
+
+require('dotenv').config();
+
+// Mongo DB
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to DB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+// Middleware
+if (process.env.NODE_ENV === 'production') {
+  app.use(helmet());
+  app.use(
+    rateLimit({
+      windowMs: 1 * 60 * 10000,
+      max: 20,
+    })
+  );
+}
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookiParser());
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port: ${process.env.PORT}`);
+});
