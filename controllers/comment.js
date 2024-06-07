93551752724cd95212c61d28c620f003c7845e89
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-06-07 14:20:44 +0530

    add: create_comment controller

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,5 +1,7 @@
 const Comment = require('../models/comment');
 const asyncHandler = require('express-async-handler');
+const comment_validators = require('../validators/comment');
+const { validationResult } = require('express-validator');
 
 exports.get_comments = asyncHandler(async function (req, res, next) {
   const comments = await Comment.find({ post: req.params.id }, { __v: 0 }).exec();
@@ -11,3 +13,20 @@ exports.get_comment = asyncHandler(async function (req, res, next) {
   res.json({ comment });
 });
 
+exports.create_comment = [
+  ...comment_validators,
+  asyncHandler(async function (req, res, next) {
+    const errors = validationResult(req);
+    const comment = new Comment({
+      text: req.body.text,
+      username: req.body.username,
+      post: req.params.id,
+    });
+    if (!errors.isEmpty()) {
+      res.json({ comment, errors: errors.array() });
+    } else {
+      await comment.save();
+      res.json({ comment });
+    }
+  }),
+];
