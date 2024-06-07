22bc43de8d2a92dc77651f6cde3da93caeffb530
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-06-07 15:44:39 +0530

    update: validates id params as optional
    now the validators can be used for all routes like post, put, delete making identifier as optional

--- validators/comment.js
+++ validators/comment.js
@@ -2,6 +2,7 @@ const { body, param } = require('express-validator');
 const { isValidObjectId } = require('mongoose');
 const Post = require('../models/post');
 const User = require('../models/user');
+const Comment = require('../models/comment');
 
 module.exports = [
   body('text').trim().isLength({ min: 1 }).withMessage('Comment cannot be empty'),
@@ -22,5 +23,12 @@ module.exports = [
       if (!isValidObjectId(val) || !(await Post.findById(val))) {
         throw new Error('Invalid Post ID');
       }
+    }),
+  param('cid')
+    .optional({ values: 'falsy' })
+    .trim()
+    .custom(async (val) => {
+      if (!isValidObjectId(val) || !(await Comment.findById(val)))
+        throw new Error('Invalid Comment ID');
     }),
 ];
--- validators/post.js
+++ validators/post.js
@@ -1,6 +1,15 @@
-const { body } = require('express-validator');
+const { body, param } = require('express-validator');
+const { isValidObjectId } = require('mongoose');
+const Post = require('../models/post');
 
 module.exports = [
   body('title').trim().isLength({ min: 1 }).withMessage('Title is required'),
   body('body').trim().isLength({ min: 7 }).withMessage('Body too short'),
+  param('id')
+    .optional({ values: 'falsy' })
+    .trim()
+    .custom(async (val) => {
+      if (!isValidObjectId(val) || !(await Post.findById(val)))
+        throw new Error('Invalid Post ID');
+    }),
 ];
