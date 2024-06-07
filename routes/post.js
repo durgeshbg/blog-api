10bab3075309a6b0535a157f25e2d501e99646a5
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-06-07 16:13:20 +0530

    add: post update route

--- routes/post.js
+++ routes/post.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.get('/', post_controller.get_posts);
 router.post('/', post_controller.create_post);
 router.get('/:id', post_controller.get_post);
+router.put('/:id', post_controller.update_post);
 
 module.exports = router;
