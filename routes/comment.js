f59dbf71506552386064216e4167f09812013ca2
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-06-07 16:26:57 +0530

    add: comment delete route

--- routes/comment.js
+++ routes/comment.js
@@ -7,5 +7,6 @@ router.get('/:id/comments', comment_controller.get_comments);
 router.post('/:id/comments', comment_controller.create_comment);
 router.get('/:id/comments/:cid', comment_controller.get_comment);
 router.put('/:id/comments/:cid', comment_controller.update_comment);
+router.delete('/:id/comments/:cid', comment_controller.delete_comment);
 
 module.exports = router;
