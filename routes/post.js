efe39ed8daf2422ffa0d1abd918e385fec89dd4b
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-06-07 14:44:24 +0530

    refactor: route to adhere REST principles 😅

--- routes/post.js
+++ routes/post.js
@@ -4,7 +4,7 @@ const post_controller = require('../controllers/post');
 const router = express.Router();
 
 router.get('/', post_controller.get_posts);
-router.post('/create', post_controller.create_post);
+router.post('/', post_controller.create_post);
 router.get('/:id', post_controller.get_post);
 
 module.exports = router;
