3c6afdd2365057ec88106f6203cba47ed3ee264b
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-06-07 12:38:10 +0530

    refactor: id -> cid get_comment route

--- routes/comment.js
+++ routes/comment.js
@@ -4,6 +4,6 @@ const comment_controller = require('../controllers/comment');
 const router = express.Router();
 
 router.get('/:id/comments', comment_controller.get_comments);
-router.get('/:id/comments/:id', comment_controller.get_comment);
+router.get('/:id/comments/:cid', comment_controller.get_comment);
 
 module.exports = router;
