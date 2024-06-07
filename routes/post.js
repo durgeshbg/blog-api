9adbfee697c671a8a94c79c2cd12173f78d48592
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-06-07 17:32:17 +0530

    add: post delete route

--- routes/post.js
+++ routes/post.js
@@ -7,5 +7,6 @@ router.get('/', post_controller.get_posts);
 router.post('/', post_controller.create_post);
 router.get('/:id', post_controller.get_post);
 router.put('/:id', post_controller.update_post);
+router.delete('/:id', post_controller.delete_post);
 
 module.exports = router;
