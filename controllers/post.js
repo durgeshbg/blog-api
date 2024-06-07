9c54ed2bcde604a035f0557ff1f6a4e9c0f27acf
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-06-07 12:18:38 +0530

    add: create_post controller

--- controllers/post.js
+++ controllers/post.js
@@ -1,5 +1,7 @@
 const Post = require('../models/post');
 const asyncHandler = require('express-async-handler');
+const post_validators = require('../validators/post');
+const { validationResult } = require('express-validator');
 
 exports.get_posts = asyncHandler(async function (req, res, next) {
   const posts = await Post.find({}, { __v: 0 });
@@ -7,6 +9,23 @@ exports.get_posts = asyncHandler(async function (req, res, next) {
 });
 
 exports.get_post = asyncHandler(async function (req, res, next) {
-  const posts = await Post.findById(req.params.id, { __v: 0 });
-  res.json({ posts: posts });
+  const post = await Post.findById(req.params.id, { __v: 0 });
+  res.json({ post });
 });
+
+exports.create_post = [
+  ...post_validators,
+  asyncHandler(async function (req, res, next) {
+    const errors = validationResult(req);
+    const post = new Post({
+      title: req.body.title,
+      body: req.body.body,
+    });
+    if (!errors.isEmpty()) {
+      res.json({ post, errors: errors.array() });
+    } else {
+      await post.save();
+      res.json({ post });
+    }
+  }),
+];
