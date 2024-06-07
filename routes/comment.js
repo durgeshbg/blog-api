db5c649c8e5372301b9547e36e4e828dfcfea11c
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-06-07 14:21:12 +0530

    add: create comment route

--- routes/comment.js
+++ routes/comment.js
@@ -4,6 +4,7 @@ const comment_controller = require('../controllers/comment');
 const router = express.Router();
 
 router.get('/:id/comments', comment_controller.get_comments);
+router.post('/:id/comments', comment_controller.create_comment);
 router.get('/:id/comments/:cid', comment_controller.get_comment);
 
 module.exports = router;
