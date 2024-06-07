2b7a0406da6035ecf2975ec123df973e3367b6b3
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-06-07 14:20:07 +0530

    add: comment validator

---
+++ validators/comment.js
@@ -0,0 +1,26 @@
+const { body, param } = require('express-validator');
+const { isValidObjectId } = require('mongoose');
+const Post = require('../models/post');
+const User = require('../models/user');
+
+module.exports = [
+  body('text').trim().isLength({ min: 1 }).withMessage('Comment cannot be empty'),
+  body('username')
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage('Username is required')
+    .custom(async (val) => {
+      if (!isValidObjectId(val) || !(await User.findById(val))) {
+        throw new Error('Invalid User ID');
+      }
+    }),
+  param('id')
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage('Post ID is required')
+    .custom(async (val) => {
+      if (!isValidObjectId(val) || !(await Post.findById(val))) {
+        throw new Error('Invalid Post ID');
+      }
+    }),
+];
