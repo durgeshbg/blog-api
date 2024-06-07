7f47a29ea716d53b7bdd7b308fad8bf58d744582
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-06-07 12:39:29 +0530

    refactor: id -> cid get_comment controller

--- controllers/comment.js
+++ controllers/comment.js
@@ -3,10 +3,11 @@ const asyncHandler = require('express-async-handler');
 
 exports.get_comments = asyncHandler(async function (req, res, next) {
   const comments = await Comment.find({ post: req.params.id }, { __v: 0 }).exec();
-  res.json({ comments: comments });
+  res.json({ comments });
 });
 
 exports.get_comment = asyncHandler(async function (req, res, next) {
-  const comments = await Comment.findById(req.params.id, { __v: 0 }).exec();
-  res.json({ comments: comments });
+  const comment = await Comment.findById(req.params.cid, { __v: 0 }).exec();
+  res.json({ comment });
 });
+
