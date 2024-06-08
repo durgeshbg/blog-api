c47b825150edf9a16b3eddd4e6d065e4f288f340
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-06-08 16:14:56 +0530

    remove: username validation

--- validators/comment.js
+++ validators/comment.js
@@ -1,20 +1,10 @@
 const { body, param } = require('express-validator');
 const { isValidObjectId } = require('mongoose');
 const Post = require('../models/post');
-const User = require('../models/user');
 const Comment = require('../models/comment');
 
 module.exports = [
   body('text').trim().isLength({ min: 1 }).withMessage('Comment cannot be empty'),
-  body('username')
-    .trim()
-    .isLength({ min: 1 })
-    .withMessage('Username is required')
-    .custom(async (val) => {
-      if (!isValidObjectId(val) || !(await User.findById(val))) {
-        throw new Error('Invalid User ID');
-      }
-    }),
   param('id')
     .trim()
     .isLength({ min: 1 })
