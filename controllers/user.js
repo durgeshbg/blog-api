a568f8243fe562bc1ed592fca1a96ed7bf7ed712
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-06-08 11:54:53 +0530

    add: passport register controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,9 @@
 const asyncHandler = require('express-async-handler');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
+const userValidators = require('../validators/user');
+const User = require('../models/user');
+const { validationResult } = require('express-validator');
 require('dotenv').config();
 
 exports.login = [
@@ -8,5 +11,26 @@ exports.login = [
   asyncHandler(async (req, res) => {
     const token = jwt.sign({ id: req.user._id }, process.env.SECRET);
     res.json({ token });
+  }),
+];
+
+exports.register = [
+  userValidators,
+  asyncHandler(async function (req, res) {
+    const errors = validationResult(req);
+    const user = new User({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    if (!errors.isEmpty()) {
+      res.json({ user, errors: errors.array() });
+    } else {
+      await user.save();
+      const token = jwt.sign({ id: user._id }, process.env.SECRET);
+      res.json({ token });
+    }
   }),
 ];
