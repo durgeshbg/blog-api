579a4f68b12902edd94509857994af0bfc094a3d
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-06-08 16:19:23 +0530

    add: isAuthor isAuthorOrAdmin middleware

--- controllers/authmiddleware.js
+++ controllers/authmiddleware.js
@@ -1,6 +1,19 @@
 const asyncHandler = require('express-async-handler');
+const Comment = require('../models/comment');
 
-exports.isAdmin = asyncHandler(async (req, res, next) => {
+exports.isAdmin = (req, res, next) => {
   if (req.user.admin) next();
   else res.status(401).json({ error: 'Not an admin' });
+};
+
+exports.isAuthor = asyncHandler(async (req, res, next) => {
+  const comment = await Comment.findById(req.params.cid);
+  if (req.user._id.toString() === comment.username.toString()) next();
+  else res.status(401).json({ error: 'You are not authorised' });
+});
+
+exports.isAuthorOrAdmin = asyncHandler(async (req, res, next) => {
+  const comment = await Comment.findById(req.params.cid);
+  if (req.user.admin || req.user._id.toString() === comment.username.toString()) next();
+  else res.status(401).json({ error: 'You are not authorised' });
 });
